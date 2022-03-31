1ca2da2d44b8fee096d652bce1c370afd945714f
Nicole Kaff <nkaff@wireless-10-104-173-2.umd.edu>
2022-03-31 10:08:09 -0400

    JS test

---
+++ code.js
@@ -0,0 +1,16 @@
+"use strict"; 
+
+window.onload = main;
+
+function main() {
+    document.querySelector("#imgButton").onclick = loadImage;
+    document.querySelector("#metaButton").onclick = loadMetadata;
+}
+
+function loadImage() {
+    document.querySelector("#imgDiv").innerHTML = '<img src="/data/imgs/001.jpeg">';
+}
+
+function loadMetadata() {
+    
+}
