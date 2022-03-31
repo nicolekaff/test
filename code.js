a1d7675ae70001fb0f12f88771af431ded65f3ac
Nicole Kaff <nkaff@wireless-10-104-173-2.umd.edu>
2022-03-31 10:49:57 -0400

    random test

--- code.js
+++ code.js
@@ -7,8 +7,18 @@ function main() {
     document.querySelector("#metaButton").onclick = loadMetadata;
 }
 
-function loadImage() {
-    document.querySelector("#imgDiv").innerHTML = '<img src="https://raw.githubusercontent.com/nicolekaff/test/main/data/imgs/001.jpeg" alt="alt text" width="400">';
+function loadImages() {
+    let randA = Math.floor(Math.random() * (4-1)+ 1);
+    let randB = Math.floor(Math.random() * (4-1)+ 1);
+    while (randA === randB) {
+        randB = Math.floor(Math.random() * (4-1)+ 1);
+    }
+    
+    let codeA = '<img src="https://raw.githubusercontent.com/nicolekaff/test/main/data/imgs/00' + String(randA) + '.jpeg">';
+    let codeB = '<img src="https://raw.githubusercontent.com/nicolekaff/test/main/data/imgs/00' + String(randB) + '.jpeg">';
+    
+    document.querySelector("#imgA").innerHTML = codeA;
+    document.querySelector("#imgB").innerHTML = codeB;
 }
 
 function loadMetadata() {
