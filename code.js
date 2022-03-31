7c29e4470ed5957e8c5a8dc4afdc8b55296addb4
Nicole Kaff <nkaff@wireless-10-104-173-2.umd.edu>
2022-03-31 10:25:20 -0400

    clean

--- code.js
+++ code.js
@@ -8,7 +8,7 @@ function main() {
 }
 
 function loadImage() {
-    document.querySelector("#imgDiv").innerHTML = '<img src="https://raw.githubusercontent.com/nicolekaff/test/main/data/imgs/001.jpeg" alt="alt text">';
+    document.querySelector("#imgDiv").innerHTML = '<img src="https://raw.githubusercontent.com/nicolekaff/test/main/data/imgs/001.jpeg" alt="alt text" width="400">';
 }
 
 function loadMetadata() {
